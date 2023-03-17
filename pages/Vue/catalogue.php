ab1f8d826260363845143d1083b89e22145bd7d0
ClemZi02 <118558165+ClemZi02@users.noreply.github.com>
2023-03-17 22:33:04 +0100

    Add files via upload

--- pages/Vue/catalogue.php
+++ pages/Vue/catalogue.php
@@ -22,12 +22,33 @@
         <?php include '../../templates/header.php'; ?>
         <hr> <!-- Repère visuel temporaire -->
         <div>
-            <h5>Produit 1 : </h5>
-            <p>image // prix : 0</p> <button>Ajouter au panier</button>
-            <h5>Produit 2 : </h5>
-            <p>image // prix : 0</p> <button>Ajouter au panier</button>
-            <h5>Produit 3 : </h5>
-            <p>image // prix : 0</p> <button>Ajouter au panier</button>
+            <h5>Casquette : </h5>
+            <div style="display: flex; align-items: center;">
+            <img src="../../img/casquette.jpg" style="width: 100px; height: 100px; margin-right: 10px;">
+            <p>Description : Casquette très utile ... notamment contre le vent<br>Prix : 10.50€</p>    
+            </div>
+            <button>Ajouter au panier</button>
+
+            <h5>Lunette de soleil : </h5>
+            <div style="display: flex; align-items: center;">
+            <img src="../../img/lunette_soleil.jpg" style="width: 100px; height: 100px; margin-right: 10px;">
+            <p>Description : Très bonne lunette de soleil qui permettent une protection très efficace contre le soleil<br>Prix : 8.99€</p>  
+            </div>
+            <button>Ajouter au panier</button>
+
+            <h5>Claquette : </h5>
+            <div style="display: flex; align-items: center;">
+            <img src="../../img/claquette.jpg" style="width: 100px; height: 100px; margin-right: 10px;">
+            <p>Description : Claquette stylée qui sont très confortables ! <br>Prix : 13.50€</p>
+            </div>
+            <button>Ajouter au panier</button>
+
+            <h5>Chaussette : </h5>
+            <div style="display: flex; align-items: center;">
+            <img src="../../img/chaussette.jpg" style="width: 100px; height: 100px; margin-right: 10px;">
+            <p>Description : Chaussette chaude à l'effigie de  l'ancien président américain Donald Trump<br>Prix : 9.50€</p>
+            </div>
+            <button>Ajouter au panier</button>
         </div>
         <hr> <!-- Repère visuel temporaire -->
         <?php include '../../templates/footer.php'; ?>
