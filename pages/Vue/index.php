f8976d641ddc7deee0c499f719c54a3930431d3d
ClemZi02 <118558165+ClemZi02@users.noreply.github.com>
2023-03-22 17:56:01 +0100

    Add files via upload

--- pages/Vue/index.php
+++ pages/Vue/index.php
@@ -3,19 +3,101 @@
 ?>
 <!DOCTYPE html>
 <html> 
-<head>
-	<meta cjharset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Accueil</title>
-	<link rel="stylesheet" href="../../css/style.css">
-	<link rel="icon" type="image/png" href="../../img/logo2.png">
-</head>
+	<head>
+		<meta cjharset="utf-8">
+		<meta name="viewport">
+		<title>Accueil</title>
+		<link rel="stylesheet" href="../../css/style.css">
+		<link rel="icon" type="image/png" href="../../img/logo2.png">
+	</head>
 <body>
 	<div>
 		<?php include '../../templates/header.php'; ?>
-		<hr> <!-- Repère visuel temporaire -->
-		<div>
-			<?php include 'catalogue.php'?>
+		<div class = "global">
+
+			<div class = "gauche">
+				<h1>Filtres</h1>
+			</div>
+
+			<div class = "box">
+				<button class ="article">
+					<img src="../../img/casquette.jpg" style="width: 100px; height: 100px; margin-right: 10px;">
+					<p>Prix : 10.50€</p>   
+					<h5>Casquette</h5> 
+				</button>
+
+				<button class ="article">
+					<img src="../../img/lunette_soleil.jpg" style="width: 100px; height: 100px; margin-right: 10px;">
+					<p>Prix : 8.99€</p>  
+					<h5>Lunette de soleil</h5>
+				</button>
+
+				<button class ="article">
+					<img src="../../img/claquette.jpg" style="width: 100px; height: 100px; margin-right: 10px;">
+					<p>Prix : 13.50€</p>
+					<h5>Claquette</h5>
+				</button>
+
+				<button class ="article">
+					<img src="../../img/chaussette.jpg" style="width: 100px; height: 100px; margin-right: 10px;">
+					<p>Prix : 9.50€</p>
+					<h5>Chaussette</h5>
+				</button>
+
+				<button class ="article">
+					<img src="../../img/casquette.jpg" style="width: 100px; height: 100px; margin-right: 10px;">
+					<p>Prix : 10.50€</p>   
+					<h5>Casquette</h5> 
+				</button>
+
+				<button class ="article">
+					<img src="../../img/lunette_soleil.jpg" style="width: 100px; height: 100px; margin-right: 10px;">
+					<p>Prix : 8.99€</p>  
+					<h5>Lunette de soleil</h5>
+				</button>
+
+				<button class ="article">
+					<img src="../../img/claquette.jpg" style="width: 100px; height: 100px; margin-right: 10px;">
+					<p>Prix : 13.50€</p>
+					<h5>Claquette</h5>
+				</button>
+
+				<button class ="article">
+					<img src="../../img/chaussette.jpg" style="width: 100px; height: 100px; margin-right: 10px;">
+					<p>Prix : 9.50€</p>
+					<h5>Chaussette</h5>
+				</button>
+
+				<button class ="article">
+					<img src="../../img/casquette.jpg" style="width: 100px; height: 100px; margin-right: 10px;">
+					<p>Prix : 10.50€</p>   
+					<h5>Casquette</h5> 
+				</button>
+
+				<button class ="article">
+					<img src="../../img/lunette_soleil.jpg" style="width: 100px; height: 100px; margin-right: 10px;">
+					<p>Prix : 8.99€</p>  
+					<h5>Lunette de soleil</h5>
+				</button>
+
+				<button class ="article">
+					<img src="../../img/claquette.jpg" style="width: 100px; height: 100px; margin-right: 10px;">
+					<p>Prix : 13.50€</p>
+					<h5>Claquette</h5>
+				</button>
+
+				<button class ="article">
+					<img src="../../img/chaussette.jpg" style="width: 100px; height: 100px; margin-right: 10px;">
+					<p>Prix : 9.50€</p>
+					<h5>Chaussette</h5>
+				</button>
+
+				<?php
+					if(isset($_SESSION["UTILISATEUR"])){
+						echo "Vous êtes connecté en tant que " . $_SESSION["UTILISATEUR"]["nom"] . " " . $_SESSION["UTILISATEUR"]["prenom"];
+					}
+				?>
+			</div>
 		</div>
 		<?php include '../../templates/footer.php'; ?>
 	</div>
