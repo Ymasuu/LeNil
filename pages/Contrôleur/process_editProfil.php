abf29cf58dd73e2bec121a34948487c3ea815fa2
Hanabi-TheFox <pintoethan@cy-tech.fr>
2023-03-27 11:48:33 +0200

    EditProfil V2

--- pages/Contrôleur/process_editProfil.php
+++ pages/Contrôleur/process_editProfil.php
@@ -16,10 +16,8 @@ if ($_POST['mdp'] != $_SESSION['UTILISATEUR']['mdp']) {
 // On récupère les informations de l'utilisateur
 if ($_POST['nom'] != "") $_SESSION['UTILISATEUR']['nom'] = $_POST['nom'];
 if ($_POST['prenom'] != "") $_SESSION['UTILISATEUR']['prenom'] = $_POST['prenom'];
-if ($_POST['email'] != "") {
-    $OLDMAIL = $_SESSION['UTILISATEUR']['email'];
-    $_SESSION['UTILISATEUR']['email'] = $_POST['email'];
-}
+$OLDMAIL = $_SESSION['UTILISATEUR']['email'];
+if ($_POST['email'] != "") $_SESSION['UTILISATEUR']['email'] = $_POST['email'];
 if ($_POST['dateNaissance'] != "") $_SESSION['UTILISATEUR']['dateNaissance'] = $_POST['dateNaissance'];
 if ($_POST['tel'] != "") $_SESSION['UTILISATEUR']['tel'] = $_POST['tel'];
 if ($_POST['adresse'] != "") $_SESSION['UTILISATEUR']['adresse'] = $_POST['adresse'];
@@ -28,12 +26,25 @@ if ($_POST['codePostal'] != "") $_SESSION['UTILISATEUR']['codePostal'] = $_POST[
 if ($_POST['pays'] != "") $_SESSION['UTILISATEUR']['pays'] = $_POST['pays'];
 
 // On met à jour les informations de l'utilisateur dans la base de données
-$utilisateurs = explode("\n", file_get_contents("../../database/client.csv"));
-foreach($utilisateurs as $end) {
-    $detailUtilisateur = explode(",", $end);
-    if($detailUtilisateur[1] == $OLDMAIL) {
-        $utilisateurs[] = implode(",", $_SESSION['UTILISATEUR']);
-        file_put_contents("../../database/client.csv", implode("\n", $utilisateurs));
+$csvFile = file_get_contents("../../database/client.csv");
+$csvArray = explode("\n", $csvFile);
+foreach($csvArray as $key => $line) {
+    $userData = explode(",", $line);
+    if($userData[2] == $OLDMAIL) {
+        // On met à jour les informations de l'utilisateur en question
+        $userData[0] = $_SESSION['UTILISATEUR']['nom'];
+        $userData[1] = $_SESSION['UTILISATEUR']['prenom'];
+        $userData[2] = $_SESSION['UTILISATEUR']['email'];
+        $userData[3] = $_SESSION['UTILISATEUR']['dateNaissance'];
+        $userData[4] = $_SESSION['UTILISATEUR']['tel'];
+        $userData[5] = $_SESSION['UTILISATEUR']['adresse'];
+        $userData[6] = $_SESSION['UTILISATEUR']['ville'];
+        $userData[7] = $_SESSION['UTILISATEUR']['codePostal'];
+        $userData[8] = $_SESSION['UTILISATEUR']['pays'];
+        $csvArray[$key] = implode(",", $userData);
+
+        $csvFile = implode("\n", $csvArray);
+        file_put_contents("../../database/client.csv", $csvFile);
         break;
     }
 }
