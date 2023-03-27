6aba36fa7f7cec096373988a2ae3e568535dde35
Abdellah427 <abdellah.hassani2002@gmail.com>
2023-03-27 12:16:56 +0200

    les drop

--- database/creationTables.sql
+++ database/creationTables.sql
@@ -1,20 +1,15 @@
 --Les tables doivent etre crées dans l'ordre suivante:
 
-CREATE TABLE Colis (
-id int not null PRIMARY key,
-longueur SMALLINT CHECK (longueur > 0),
-hauteur SMALLINT CHECK (hauteur > 0);
-poids SMALLINT CHECK(poids > 0)
-);
+DROP DATABASE IF EXISTS Marketplace;  -- creer la base avant
+CREATE DATABASE Marketplace;
+USE Marketplace;  -- se connecter a la base
 
-CREATE TABLE Commande (
-    id int not null PRIMARY KEY,
-    totalPayer int CHECK (not(totalPayer < 0)),
-    modePayment varchar(20),
-    datePayment date,
-    idColis int FOREIGN KEY REFERENCES Colis(id),
-
-);
+DROP TABLE IF EXISTS Client;  -- rajouter IF EXISTS
+DROP TABLE IF EXISTS Panier;
+DROP TABLE IF EXISTS Colis;
+DROP TABLE IF EXISTS Commande;
+DROP TABLE IF EXISTS ProduitsVendeur;
+DROP TABLE IF EXISTS Recherche;
 
 CREATE TABLE Client (
 prenom varchar(50) not null,
@@ -31,7 +26,42 @@ idCommande int,
     FOREIGN KEY (idCommande) REFERENCES Commande(id)
 );
 
-CREATE TABLE RECHRCHE(
+CREATE TABLE Panier (
+     int not null PRIMARY KEY,
+    HT decimal,
+    TVA decimal,
+    TTC decimal CHECK (TTC >= HT)
+    idClient int FOREIGN KEY REFERENCES Client(id),
+    
+);
+
+CREATE TABLE Colis (
+id int not null PRIMARY key,
+longueur SMALLINT CHECK (longueur > 0),
+hauteur SMALLINT CHECK (hauteur > 0);
+poids SMALLINT CHECK(poids > 0)
+);
+
+CREATE TABLE Commande (
+    id int not null PRIMARY KEY,
+    totalPayer int CHECK (not(totalPayer < 0)),
+    modePayment varchar(20),
+    datePayment date,
+    idColis int FOREIGN KEY REFERENCES Colis(id),
+
+);
+
+CREATE TABLE ProduitsVendeur (
+id int not null PRIMARY KEY,
+QuantiteVendeur int not null,
+prix decimal not null,
+FOREIGN KEY (idPanier) REFERENCES Panier(id)
+);
+
+
+
+
+CREATE TABLE Recherche(
     motCle varchar(255) not null PRIMARY KEY,
     
 );
