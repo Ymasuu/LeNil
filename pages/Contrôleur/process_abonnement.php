db48f822376e6081d175ee481b76d352146697f8
Ymasuu <samy.belbouab95@gmail.com>
2023-03-27 13:30:17 +0200

    NICEEEEE

--- pages/Contrôleur/process_abonnement.php
+++ pages/Contrôleur/process_abonnement.php
@@ -1,28 +1,32 @@
 <?php
     session_start();
     if(isset($_POST['mensuel'])){
-        $_SESSION['UTILISATEUR']['Abonnement'] = "Abonnement Mensuel";
+        $_SESSION["UTILISATEUR"]["Abonnement"] = "Abonnement Mensuel";
     }
     if(isset($_POST['annuel'])){
-       $_SESSION['UTILISATEUR']['Abonnement'] = "Abonnement Annuel";
+        $_SESSION["UTILISATEUR"]["Abonnement"] = "Abonnement Annuel";
     }
 
 
-    /*$fichier = file_get_contents("../../database/client.csv");
-    $utilisateur = explode("\n", $fichier);
-    foreach($utilisateur as $end) {
-        if ($utilisateur[2] == $_SESSION["UTILISATEUR"]["email"]) {
-            // Mettre à jour les données dans le tableau $ligne
-            $utilisateur[10] = "test";
-            header('Location:../Vue/profil.php');
-            exit();
-        }
-        $fichier = implode("\n", $csvArray);
-        file_put_contents("../../database/client.csv", $fichier);
-        break;
-    }*/
+    // On met à jour les informations de l'utilisateur dans la base de données
+    $csvFile = file_get_contents("../../database/client.csv");
+    $csvArray = explode("\n", $csvFile);
+    foreach($csvArray as $key => $line) {
+        $userData = explode(",", $line);
+        if($userData[2] == $_SESSION["UTILISATEUR"]["email"]) {
+            // On met à jour l'informations de l'utilisateur en question
+            $userData[10] = $_SESSION["UTILISATEUR"]["Abonnement"];
+            $csvArray[$key] = implode(",", $userData);
 
-    $csv = file_get_contents("../../database/client.csv");
+            $csvFile = implode("\n", $csvArray);
+            file_put_contents("../../database/client.csv", $csvFile);
+            break;
+        }
+    }
+
+
+
+    /*$csv = file_get_contents("../../database/client.csv");
     // diviser la chaîne en lignes
     $lignes = explode("\n", $csv);
 
@@ -32,8 +36,8 @@
         $colonnes = str_getcsv($ligne, ',');
 
         // si la colonne 3 contient votre adresse e-mail, mettre à jour la colonne 9 avec la nouvelle donnée
-        if ($colonnes[2] == $_SESSION['UTILISATEUR']['email']) {
-            $colonnes[10] = $_SESSION['UTILISATEUR']['Abonnement'];
+        if ($colonnes[2] == $_SESSION["UTILISATEUR"]["email"]) {
+            $colonnes[10] = $_SESSION["UTILISATEUR"]["Abonnement"];
             // fusionner les colonnes en une nouvelle ligne
             $nouvelle_ligne = implode(',', $colonnes);
             // remplacer la ligne d'origine par la nouvelle ligne
@@ -43,13 +47,13 @@
             // fusionner les lignes en une chaîne de caractères
             $nouveau_csv = implode("\n", $lignes);
             // écrire le nouveau contenu dans le fichier
-            file_put_contents('chemin/vers/votre/fichier.csv', $nouveau_csv);
+            exit();
             break;
         }        
-    }
+    }*/
 
 
-
+    $_SESSION["merci"] = "merci pour votre abonnement";
     header('Location:../Vue/index.php');
     exit();
 ?>
