278b33f0754d59b2bc2bd05da3649fcfd1d69e8b
Ymasuu <samy.belbouab95@gmail.com>
2023-03-27 12:11:31 +0200

    0

--- pages/Contrôleur/process_abonnement.php
+++ pages/Contrôleur/process_abonnement.php
@@ -7,21 +7,40 @@
        $_SESSION['UTILISATEUR']['Abonnement'] = "Abonnement Annuel";
     }
 
-    $fichier = fopen('../../database/client.csv', 'r+');
-    // Parcours du fichier CSV et mise à jour des enregistrements correspondants
-    while (($utilisateur = fgetcsv($fichier)) !== FALSE) {
+
+    /*$csvFile = file_get_contents("../../database/client.csv");
+    $csvArray = explode("\n", $csvFile);
+    foreach($csvArray as $key => $line) {
+        $utilisateur = explode(",", $line);
         if ($utilisateur[2] == $_SESSION['UTILISATEUR']['email'] && $utilisateur[9] == $_SESSION['UTILISATEUR']['mdp']) {
             // Mettre à jour les données dans le tableau $ligne
             $utilisateur[10] = $_SESSION['UTILISATEUR']['Abonnement'];
-            // Réécrire la ligne mise à jour dans le fichier CSV
-            fseek($fichier, -strlen(implode(',', $ligne)), SEEK_CUR);
+        }
+        $csvArray[$key] = implode(",", $utilisateur);
+
+        $csvFile = implode("\n", $csvArray);
+        file_put_contents("../../database/client.csv", $csvFile);
+        break;
+    }*/
+
+    $fichier = fopen("../../database/client.csv", 'r+');
+    // Boucle pour parcourir les lignes du fichier CSV
+    while (($utilisateur = fgetcsv($fichier)) !== false) {
+        
+        // Vérification de l'identifiant de la ligne courante
+        if ($utilisateur[2] == $_SESSION['UTILISATEUR']['email'] && $utilisateur[9] == $_SESSION['UTILISATEUR']['mdp']) {
+            
+            // Modification des données de la ligne courante
+            $utilisateur[10] = $_SESSION['UTILISATEUR']['Abonnement'];
+            
+            // Réécriture de la ligne courante dans le fichier CSV
+            fseek($fichier, -1, SEEK_CUR);
             fputcsv($fichier, $utilisateur);
             break;
         }
     }
     // Fermeture du fichier CSV
     fclose($fichier);
-
 
     header('Location: ../Vue/profil.php');
     exit();
