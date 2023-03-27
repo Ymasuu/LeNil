c8839afbbce36b3b066118b73ba776375ffeb156
RDNATOS <78343596+RDNATOS@users.noreply.github.com>
2023-03-27 11:32:32 +0200

    Debut creration des tables pour la database

---
+++ database/creationTables.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Colis (
+id int not null PRIMARY key,
+longueur SMALLINT CHECK (longueur > 0),
+hauteur SMALLINT CHECK (hauteur > 0);
+poids SMALLINT CHECK(poids > 0)
+);
+
+CREATE TABLE Commande (
+    id int not null PRIMARY KEY,
+    totalPayer int CHECK (not(totalPayer < 0)),
+    modePayment varchar(20),
+    datePayment date,
+    idColis int FOREIGN KEY REFERENCES Colis(id),
+    
+);
+
+CREATE TABLE Client (
+prenom varchar(50) not null,
+nom varchar(50) not null,
+email varchar(150) not null PRIMARY KEY,
+dateNaissance date not null,
+telephone int not null,
+adresse varchar(250) not null,
+ville varchar(50) not null,
+codePostal SMALLINT not null,
+pays varchar(50) not null,
+motDePasse varchar(100) not null,
+idCommande int,
+    FOREIGN KEY REFERENCES Commande(idCommande)
+);
+
