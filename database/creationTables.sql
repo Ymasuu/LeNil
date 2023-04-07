0f2dfb7a43ec00199b453140a6aa7b9c9893bb72
Abdellah <95984944+Abdellah427@users.noreply.github.com>
2023-04-07 14:57:09 +0200

    Update creationTables.sql
    Résolution de bug

--- database/creationTables.sql
+++ database/creationTables.sql
@@ -12,66 +12,72 @@ DROP TABLE IF EXISTS ProduitsVendeur;
 DROP TABLE IF EXISTS Recherche;
 DROP TABLE IF EXISTS Produit;
 
-CREATE TABLE Panier (
-    int not null PRIMARY KEY,
-    HT decimal,
-    TVA decimal,
-    TTC decimal CHECK (TTC >= HT),
-    idClient int FOREIGN KEY REFERENCES Client(id),
-    
+CREATE TABLE Colis (
+    id int not null PRIMARY KEY,
+    longueur SMALLINT CHECK (longueur > 0),
+    hauteur SMALLINT CHECK (hauteur > 0),
+    poids SMALLINT CHECK (poids > 0)
 );
 
-CREATE TABLE Colis (
-id int not null PRIMARY KEY,
-longueur SMALLINT CHECK (longueur > 0),
-hauteur SMALLINT CHECK (hauteur > 0),
-poids SMALLINT CHECK(poids > 0)
-);
+
 
 CREATE TABLE Commande (
     id int not null PRIMARY KEY,
     totalPayer int CHECK (not(totalPayer < 0)),
     modePayment varchar(20),
     datePayment date,
-    idColis int FOREIGN KEY REFERENCES Colis(id),
+    idColis int REFERENCES Colis(id)
+    
 
 );
 
 CREATE TABLE Client (
-prenom varchar(50) not null,
-nom varchar(50) not null,
-email varchar(150) not null PRIMARY KEY,
-dateNaissance date not null,
-telephone int not null,
-adresse varchar(250) not null,
-ville varchar(50) not null,
-codePostal SMALLINT not null,
-pays varchar(50) not null,
-motDePasse varchar(100) not null,
-idCommande int,
-    FOREIGN KEY (idCommande) REFERENCES Commande(id)
+    email varchar(150) not null PRIMARY KEY,
+    prenom varchar(50) not null,
+    nom varchar(50) not null,
+    dateNaissance date not null,
+    telephone int not null,
+    adresse varchar(250) not null,
+    ville varchar(50) not null,
+    codePostal SMALLINT not null,
+    pays varchar(50) not null,
+    motDePasse varchar(100) not null,
+    idCommande int REFERENCES Commande(id)
 );
 
-CREATE TABLE ProduitsVendeur (
-id int not null PRIMARY KEY,
-QuantiteVendeur int not null,
-prix decimal not null,
-FOREIGN KEY (idPanier) REFERENCES Panier(id)
-);
 
-CREATE TABLE Recherche(
-    motCle varchar(255) not null PRIMARY KEY,
+CREATE TABLE Panier (
+    id int not null PRIMARY KEY,
+    HT decimal,
+    TVA decimal,
+    TTC decimal,
+    emailClient varchar(150) REFERENCES Client(email)
     
 );
-CREATE TABLE Caracteristique{
+
+
+
+CREATE TABLE ProduitsVendeur (
+    id int not null PRIMARY KEY,
+    QuantiteVendeur int not null,
+    prix decimal not null,
+    idPanier int REFERENCES Panier(id)
+);
+
+
+CREATE TABLE Recherche(
+    motCle varchar(255) not null PRIMARY KEY
+    
+);
+
+CREATE TABLE Caracteristique(
     id int not null PRIMARY KEY,
     valeur varchar(10) not null,
-    nom varchar(50) not null,
-};
+    nom varchar(50) not null
+);
 
-CREATE TABLE Produit{
+CREATE TABLE Produit(
     id int not null PRIMARY KEY,
     description varchar(255) not null,
-    FOREIGN KEY (idCaracteristique) REFERENCES Caracteristique(id
-};
-
+    idCaracteristique int REFERENCES Caracteristique(id)
+);
