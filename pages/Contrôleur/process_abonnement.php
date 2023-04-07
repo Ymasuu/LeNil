5a2add429a9ea028f55d1dda8917bc5382464c2e
Ymasuu <samy.belbouab95@gmail.com>
2023-04-07 14:01:41 +0200

    dataAbonnement

--- pages/Contrôleur/process_abonnement.php
+++ pages/Contrôleur/process_abonnement.php
@@ -1,17 +1,22 @@
 <?php
     session_start();
 
+    date_default_timezone_set('Europe/Paris');
+    $date = date('d-m-y');
+
     if(isset($_POST['desabonner'])){
         $_SESSION["UTILISATEUR"]["Abonnement"] = "None";
+        $_SESSION["UTILISATEUR"]["DateAbonnement"] = "None";
         $_SESSION["merci"] = "Nous vous confirmons la résilation de votre abonnement. Vous pouvez vous réabonner quand vous voulez !";
-
     }
     else {
         if(isset($_POST['mensuel'])){
             $_SESSION["UTILISATEUR"]["Abonnement"] = "Abonnement Mensuel";
+            $_SESSION["UTILISATEUR"]["DateAbonnement"] = $date;
         }
         if(isset($_POST['annuel'])){
             $_SESSION["UTILISATEUR"]["Abonnement"] = "Abonnement Annuel";
+            $_SESSION["UTILISATEUR"]["DateAbonnement"] = $date;
         }
         $_SESSION["merci"] = "merci pour votre abonnement !";
     }
@@ -24,6 +29,7 @@
         if($userData[2] == $_SESSION["UTILISATEUR"]["email"]) {
             // On met à jour l'informations de l'utilisateur en question
             $userData[10] = $_SESSION["UTILISATEUR"]["Abonnement"];
+            $userData[11] = $_SESSION["UTILISATEUR"]["DateAbonnement"];
             $csvArray[$key] = implode(",", $userData);
 
             $csvFile = implode("\n", $csvArray);
