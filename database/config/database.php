b61e1ae4cd73cedba55dbd77a3fdbac404e482e9
RDNATOS <78343596+RDNATOS@users.noreply.github.com>
2023-04-13 17:08:27 +0200

    rien d'important

---
+++ database/config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'cytech0001');
+define('DB_NAME', 'lenil');
+
+
+$dbServerName = "localhost";
+$dbUserName="root";
+$dbName = "lenil";
+?>
