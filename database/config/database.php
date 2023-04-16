894455bef5ed05fa64e5d14e1b47db4c511e43d3
RDNATOS <78343596+RDNATOS@users.noreply.github.com>
2023-04-16 15:14:33 +0200

    qq modification

--- database/config/database.php
+++ database/config/database.php
@@ -5,8 +5,11 @@ define('DB_USER', 'root');
 define('DB_PASS', 'cytech0001');
 define('DB_NAME', 'lenil');
 
-
 $dbServerName = "localhost";
 $dbUserName="root";
 $dbName = "lenil";
+
+//CLIENT//////////////
+//Une fois le client connectée, il aura accés à son compte
+//et au panier
 ?>
