f5dee09513e88fda13c21298605dc17e1f9859f1
Ymasuu <samy.belbouab95@gmail.com>
2023-04-19 00:04:11 +0200

    process abonnement avec bdd

--- pages/Contrôleur/process_abonnement.php
+++ pages/Contrôleur/process_abonnement.php
@@ -1,26 +1,48 @@
 <?php
+    require_once '..\..\database\config\connection.php';
+    require_once '..\..\database\config\database.php';
     session_start();
 
     date_default_timezone_set('Europe/Paris');
-    $date = date('d-m-Y');
+    $date = date('Y-m-d');
 
     if(!isset($_POST['mensuel']) and !isset($_POST['annuel']) ){
-        $_SESSION["UTILISATEUR"]["Abonnement"] = "0";
+        $_SESSION["UTILISATEUR"]["Abonnement"] = "None";
+        $abo = 0;
         $_SESSION["UTILISATEUR"]["DateAbonnement"] = "None";
         $_SESSION["merci"] = "Nous vous confirmons la résilation de votre abonnement. Vous pouvez vous réabonner quand vous voulez !";
     }
     else {
         if(isset($_POST['mensuel'])){
             $_SESSION["UTILISATEUR"]["Abonnement"] = "Abonnement Mensuel";
+            $abo = 1;
             $_SESSION["UTILISATEUR"]["DateAbonnement"] = $date;
         }
         if(isset($_POST['annuel'])){
             $_SESSION["UTILISATEUR"]["Abonnement"] = "Abonnement Annuel";
+            $abo = 2;
             $_SESSION["UTILISATEUR"]["DateAbonnement"] = $date;
         }
         $_SESSION["merci"] = "merci pour votre abonnement !";
     }
 
+    
+    $mail = $_SESSION["UTILISATEUR"]["email"];
+    $password = $_SESSION["UTILISATEUR"]["mdp"];
+
+    //ACCES DE LUTILISATEUR DANS LA BDD
+    // Escape special characters to prevent SQL injection attacks
+    $mail = mysqli_real_escape_string($conn, $mail);
+    $password = mysqli_real_escape_string($conn, $password);
+
+    // Execute the SQL query with escaped values
+    $query = "UPDATE Compte SET abonnement = '$abo' WHERE email='$mail' AND motDePasse='$password';";
+    $result = mysqli_query($conn, $query);
+    if($abo != 0)$query = "UPDATE Compte SET dateAbonnement = '$date' WHERE email='$mail' AND motDePasse='$password';";
+    else $query = "UPDATE Compte SET dateAbonnement = NULL WHERE email='$mail' AND motDePasse='$password';";
+    $result = mysqli_query($conn, $query);
+
+    /*
     // On met à jour les informations de l'utilisateur dans la base de données
     $csvFile = file_get_contents("../../database/client.csv");
     $csvArray = explode("\n", $csvFile);
@@ -36,7 +58,7 @@
             file_put_contents("../../database/client.csv", $csvFile);
             break;
         }
-    }
+    }*/
 
     header('Location:../Vue/index.php');
     exit();
