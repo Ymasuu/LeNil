2f4fb3098b6099a0508b471f8c04f1dc407db71f
ClemZi02 <clement.foulon19@gmail.com>
2023-04-19 00:12:02 +0200

    affichageProdVend

--- pages/Vue/Vendeur.php
+++ pages/Vue/Vendeur.php
@@ -13,37 +13,42 @@
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="icon" type="image/png" href="../../img/logo2.png">
 </head>
-
 <body>
     <div>
         <?php include '../../templates/header.php'; ?>
         <hr> <!-- Repère visuel temporaire -->
         <div>
-            <div class = "box">
+            <div class="box">
                 <?php
-                    // Récupération de l'ID du vendeur connecté à partir de la session
-                    $emailCompte = $_SESSION['emailCompte'];
-
-                    // Récupération des produits du vendeur connecté
-                    $resultat = mysqli_query($conn, "SELECT * FROM produitsvendeur WHERE idVendeur = $emailCompte");
-
-                    // Affichage des produits
-                    while ($produit = mysqli_fetch_assoc($resultat)) {
+                    // Vérifier si la variable de session 'email' existe
+                    if (isset($_SESSION["UTILISATEUR"]["email"])) {
+                        // Requête pour récupérer les informations de chaque produit du vendeur connecté
+                        $email = $_SESSION["UTILISATEUR"]["email"];
+                        $resultat = mysqli_query($conn, "SELECT * FROM produitsvendeur WHERE emailVendeur = '$email'");
+                        //Parcours des résultats avec une boucle while
+                        while ($produit = mysqli_fetch_assoc($resultat)) {
                 ?>
-                <button class="article">
-                    <img src="../../img/<?php echo $produit['NomImage']; ?>" style="width: 100px; height: 100px; margin-right: 10px;">
-                    <div>
-                        <h5><?php echo $produit['nom']; ?></h5>
-                        <p><?php echo $produit['minidescription']; ?></p>
-                        <p>Prix : <?php echo $produit['prix']; ?> €</p>
-                    </div>
-                </button>
+                            <button class="article">
+                                <img src="../../img/<?php echo $produit['NomImage']; ?>" style="width: 100px; height: 100px; margin-right: 10px;">
+                                <div>
+                                    <h5><?php echo $produit['nom']; ?></h5>
+                                    <p><?php echo $produit['minidescription']; ?></p>
+                                    <p>Prix : <?php echo $produit['prix']; ?> €</p>
+                                </div>
+                            </button>
                 <?php
+                        }
+                        // Fermeture de la connexion à la base de données
+                        mysqli_close($conn);
+                    } else {
+                        echo "Vous n'êtes pas connecté.";
                     }
                 ?>
-			</div>
+            </div>
+            <hr> <!-- Repère visuel temporaire -->
+            <?php include '../../templates/footer.php'; ?>
         </div>
-        <hr> <!-- Repère visuel temporaire -->
-        <?php include '../../templates/footer.php'; ?>
     </div>
 </body>
+</html>
+
--- pages/Vue/index.php
+++ pages/Vue/index.php
@@ -24,34 +24,35 @@
 			if(isset($_SESSION["merci"]))echo "<center><b>".$_SESSION["merci"]."</b></center>"; unset($_SESSION["merci"]);
 		?>
 		<div class = "global">
-
 			<div class = "gauche">
 				<h1>Filtres</h1>
 			</div>
 			<div class = "box">
-			<?php
-// Requête pour récupérer les informations de chaque produit
-$resultat = mysqli_query($conn, "SELECT * FROM produitsvendeur");
+                <?php
+                    // Requête pour récupérer les informations de chaque produit
+                    $resultat = mysqli_query($conn, "SELECT * FROM produitsvendeur");
 
-//Parcours des résultats avec une boucle while
-while ($produit = mysqli_fetch_assoc($resultat)) {
-?>
-    <button class="article">
-        <img src="../../img/<?php echo $produit['NomImage']; ?>" style="width: 100px; height: 100px; margin-right: 10px;">
-        <div>
-            <h5><?php echo $produit['nom']; ?></h5>
-            <p><?php echo $produit['minidescription']; ?></p>
-            <p>Prix : <?php echo $produit['prix']; ?> €</p>
-        </div>
-    </button>
-<?php
-}
-// Fermeture de la connexion à la base de données
-mysqli_close($conn);
-?>
-
-			</div>
+                    //Parcours des résultats avec une boucle while
+                    while ($produit = mysqli_fetch_assoc($resultat)) {
+                ?>
+                <button class="article">
+                    <img src="../../img/<?php echo $produit['NomImage']; ?>" style="width: 100px; height: 100px; margin-right: 10px;">
+                    <div>
+                        <h5><?php echo $produit['nom']; ?></h5>
+                        <p><?php echo $produit['minidescription']; ?></p>
+                        <p>Prix : <?php echo $produit['prix']; ?> €</p>
+                    </div>
+                </button>
+                <?php
+                }
+                // Fermeture de la connexion à la base de données
+                mysqli_close($conn);
+                ?>
+            </div>
 		</div>
+		<form action="Vendeur.php">
+			<input type="submit" value="Gérer Produit">
+		</form>
 		<?php include '../../templates/footer.php'; ?>
 	</div>
 </body>
