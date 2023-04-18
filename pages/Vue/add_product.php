0241e05a549a8cedc3422b28832450d3e73582a2
Abdellah <95984944+Abdellah427@users.noreply.github.com>
2023-04-19 00:09:47 +0200

    Update add_product.php

--- pages/Vue/add_product.php
+++ pages/Vue/add_product.php
@@ -10,7 +10,7 @@
             <h1>Ajouter une annonce</h1>
         </header>
         <main>
-            <form method="post">
+            <form method="post" enctype="multipart/form-data">
                 <div>
                     <label for="titre">Titre :</label>
                     <input type="text" id="titre" name="titre" required>
@@ -24,34 +24,46 @@
                     <input type="number" id="prix" name="prix" required>
                 </div>
                 <div>
+                    <label for="image">Image :</label>
+                    <input type="file" id="image" name="image" accept="image/*">
+                </div>
+                <div>
                     <button class="bouton-golden" type="submit" name="submit">Ajouter</button>
                 </div>
             </form>
         </main>
-        <footer>
-            
-            <hr>
-            
-        </footer>
     </body>
 </html>
 
 <?php
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
-	// Traitement du formulaire
-	$titre = $_POST['titre'];
-	$description = $_POST['description'];
-	$prix = $_POST['prix'];
+if (isset($_POST['submit'])) {
+    $titre = $_POST['titre'];
+    $description = $_POST['description'];
+    $prix = $_POST['prix'];
+    $image = $_FILES['image'];
 
-	// Validation des données
-	if(empty($titre) || empty($description) || empty($prix)) {
-		echo "Veuillez remplir tous les champs";
-	} else {
-		// Insertion de l'annonce dans la base de données
-		$bdd = new PDO('mysql:host=localhost;dbname=lenil;charset=utf8', 'root', '');
-		$req = $bdd->prepare('INSERT INTO ProduitVendeur (nom, description, prix) VALUES (?, ?, ?)');
-		$req->execute(array($titre, $description, $prix));
-		echo "Annonce ajoutée avec succès !";
-	}
+    // Vérifier si l'utilisateur a choisi un fichier
+    if ($image['name'] !== '') {
+        // Vérifier si le fichier est une image
+        if (getimagesize($image['tmp_name'])) {
+            // Générer un nom de fichier unique
+            $filename = uniqid('', true) . '.' . pathinfo($image['name'], PATHINFO_EXTENSION);
+            // Enregistrer l'image sur le serveur
+            move_uploaded_file($image['tmp_name'], 'uploads/' . $filename);
+            // Enregistrer les données de l'annonce dans la base de données
+            // ...
+            // Rediriger vers la page d'accueil
+            header('Location: index.php');
+            exit;
+        } else {
+            echo 'Le fichier choisi n\'est pas une image.';
+        }
+    } else {
+        // Enregistrer les données de l'annonce dans la base de données
+        // ... A faire
+        // Rediriger vers la page d'accueil
+        header('Location: index.php');
+        exit;
+    }
 }
 ?>
