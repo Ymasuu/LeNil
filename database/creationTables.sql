a8c7f845055e80dee6d6e1af7058ca0cb02b9182
Ymasuu <samy.belbouab95@gmail.com>
2023-04-19 22:26:06 +0200

    sync

--- database/creationTables.sql
+++ database/creationTables.sql
@@ -298,8 +298,8 @@ CREATE TABLE `ProduitsVendeur` (
 --
 
 INSERT INTO `ProduitsVendeur` (`id`, `emailVendeur`, `QuantiteVendeur`, `prix`, `NomImage`, `nom`, `description`, `minidescription`, `categorie`) VALUES
-(1, 'magasin1@gmail.com', 100, '29.99', 'casque.png', 'Casque', 'Grâce à la nouvelle conception de son boîtier de commande audio USB et sa carte son intégrée, HyperX Cloud II amplifie l\'audio et la voix pour créer une expérience de jeu Hi-Fi optimale. Vous serez toujours certain de tout entendre. Vous accédez à un monde de détails que les autres gamers ne connaîtront jamais : le bruissement d\'une botte dans les feuilles, une course furtive dans une conduite. Les commandes indépendantes permettent de régler le volume de l\'écoute et le niveau du micro. Vous pouvez aussi activer le son Surround 7.1 ou le micro.', 'Casque bluetooth', 'Informatique'),
-(2, 'magasin1@gmail.com', 150, '19.99', 'casquette.jpg', 'Casquette', 'Balinco Casquette d\'hélice colorée | Casquette d\'hélicoptère | Casquette de propulseur | Casquette de Baseball pour Adultes et Enfants - Taille Ajustable', 'Accessoire de mode', 'Vêtement'),
+(1, 'magasin1@gmail.com', 100, '29.99', 'casque.png', 'Casque', 'Grâce à la nouvelle conception de son boîtier de commande audio USB et sa carte son intégrée, HyperX Cloud II amplifie l''audio et la voix pour créer une expérience de jeu Hi-Fi optimale. Vous serez toujours certain de tout entendre. Vous accédez à un monde de détails que les autres gamers ne connaîtront jamais : le bruissement d''une botte dans les feuilles, une course furtive dans une conduite. Les commandes indépendantes permettent de régler le volume de l''écoute et le niveau du micro. Vous pouvez aussi activer le son Surround 7.1 ou le micro.', 'Casque bluetooth', 'Informatique'),
+(2, 'magasin1@gmail.com', 150, '19.99', 'casquette.jpg', 'Casquette', 'Balinco Casquette d''hélice colorée | Casquette d''hélicoptère | Casquette de propulseur | Casquette de Baseball pour Adultes et Enfants - Taille Ajustable', 'Accessoire de mode', 'Vêtement'),
 (3, 'magasin1@gmail.com', 200, '9.99', 'chaussette.jpg', 'Chaussette', NULL, 'Chaussette confortable', 'Vêtement'),
 (4, 'magasin2@gmail.com', 150, '19.99', 'casquette.jpg', 'Casquette', NULL, 'Accessoire de mode', 'Vêtement'),
 (5, 'magasin1@gmail.com', 55, '29.99', 'clavier.jpg', 'Clavier', NULL, 'Clavier de jeu', 'Informatique'),
@@ -314,7 +314,7 @@ INSERT INTO `ProduitsVendeur` (`id`, `emailVendeur`, `QuantiteVendeur`, `prix`, 
 (14, 'magasin1@gmail.com', 54, '6.99', 'playmobil2.jpeg', 'Playmobil 2.0', NULL, 'Pour vos enfants', 'Jeux pour enfant'),
 (15, 'magasin1@gmail.com', 865, '13.99', 'souris.jpg', 'Souris', NULL, 'Souris de jeu', 'Informatique'),
 (16, 'magasin1@gmail.com', 254, '949.99', 'tour.jpg', 'Tour PC', NULL, 'Ordinateur pour gamer', 'Informatique'),
-(17, 'magasin1@gmail.com', 55, '29.99', 'claquette.jpg', 'Claquette', NULL, 'Chaussure d\'été', 'Vêtement');
+(17, 'magasin1@gmail.com', 55, '29.99', 'claquette.jpg', 'Claquette', NULL, 'Chaussure d''été', 'Vêtement');
 
 -- --------------------------------------------------------
 
