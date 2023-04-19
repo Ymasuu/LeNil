a9d31b02f82530677a0433d1866e9d11a5153d2c
Abdellah <95984944+Abdellah427@users.noreply.github.com>
2023-04-19 22:24:45 +0200

    Update creationTables.sql

--- database/creationTables.sql
+++ database/creationTables.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mar. 18 avr. 2023 à 13:56
+-- Généré le : mer. 19 avr. 2023 à 22:22
 -- Version du serveur :  8.0.32-0ubuntu0.20.04.2
 -- Version de PHP : 7.4.3-4ubuntu2.18
 
@@ -11,6 +11,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -288,7 +289,7 @@ CREATE TABLE `ProduitsVendeur` (
   `NomImage` char(50) NOT NULL,
   `nom` char(100) NOT NULL,
   `description` text,
-  `minidescription` varchar(200) NOT NULL
+  `minidescription` varchar(200) NOT NULL,
   `categorie` enum('Informatique','Jeux pour enfant','Vêtement') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -296,10 +297,9 @@ CREATE TABLE `ProduitsVendeur` (
 -- Déchargement des données de la table `ProduitsVendeur`
 --
 
-
 INSERT INTO `ProduitsVendeur` (`id`, `emailVendeur`, `QuantiteVendeur`, `prix`, `NomImage`, `nom`, `description`, `minidescription`, `categorie`) VALUES
-(1, 'magasin1@gmail.com', 100, '29.99', 'casque.png', 'Casque', 'Grâce à la nouvelle conception de son boîtier de commande audio USB et sa carte son intégrée, HyperX Cloud II amplifie l''audio et la voix pour créer une expérience de jeu Hi-Fi optimale. Vous serez toujours certain de tout entendre. Vous accédez à un monde de détails que les autres gamers ne connaîtront jamais : le bruissement d''une botte dans les feuilles, une course furtive dans une conduite. Les commandes indépendantes permettent de régler le volume de l''écoute et le niveau du micro. Vous pouvez aussi activer le son Surround 7.1 ou le micro.', 'Casque bluetooth', 'Informatique'),
-(2, 'magasin1@gmail.com', 150, '19.99', 'casquette.jpg', 'Casquette', 'Balinco Casquette d''hélice colorée | Casquette d''hélicoptère | Casquette de propulseur | Casquette de Baseball pour Adultes et Enfants - Taille Ajustable', 'Accessoire de mode', 'Vêtement'),
+(1, 'magasin1@gmail.com', 100, '29.99', 'casque.png', 'Casque', 'Grâce à la nouvelle conception de son boîtier de commande audio USB et sa carte son intégrée, HyperX Cloud II amplifie l\'audio et la voix pour créer une expérience de jeu Hi-Fi optimale. Vous serez toujours certain de tout entendre. Vous accédez à un monde de détails que les autres gamers ne connaîtront jamais : le bruissement d\'une botte dans les feuilles, une course furtive dans une conduite. Les commandes indépendantes permettent de régler le volume de l\'écoute et le niveau du micro. Vous pouvez aussi activer le son Surround 7.1 ou le micro.', 'Casque bluetooth', 'Informatique'),
+(2, 'magasin1@gmail.com', 150, '19.99', 'casquette.jpg', 'Casquette', 'Balinco Casquette d\'hélice colorée | Casquette d\'hélicoptère | Casquette de propulseur | Casquette de Baseball pour Adultes et Enfants - Taille Ajustable', 'Accessoire de mode', 'Vêtement'),
 (3, 'magasin1@gmail.com', 200, '9.99', 'chaussette.jpg', 'Chaussette', NULL, 'Chaussette confortable', 'Vêtement'),
 (4, 'magasin2@gmail.com', 150, '19.99', 'casquette.jpg', 'Casquette', NULL, 'Accessoire de mode', 'Vêtement'),
 (5, 'magasin1@gmail.com', 55, '29.99', 'clavier.jpg', 'Clavier', NULL, 'Clavier de jeu', 'Informatique'),
@@ -314,8 +314,7 @@ INSERT INTO `ProduitsVendeur` (`id`, `emailVendeur`, `QuantiteVendeur`, `prix`, 
 (14, 'magasin1@gmail.com', 54, '6.99', 'playmobil2.jpeg', 'Playmobil 2.0', NULL, 'Pour vos enfants', 'Jeux pour enfant'),
 (15, 'magasin1@gmail.com', 865, '13.99', 'souris.jpg', 'Souris', NULL, 'Souris de jeu', 'Informatique'),
 (16, 'magasin1@gmail.com', 254, '949.99', 'tour.jpg', 'Tour PC', NULL, 'Ordinateur pour gamer', 'Informatique'),
-(17, 'magasin1@gmail.com', 55, '29.99', 'claquette.jpg', 'Claquette', NULL, 'Chaussure d''été', 'Vêtement');
-
+(17, 'magasin1@gmail.com', 55, '29.99', 'claquette.jpg', 'Claquette', NULL, 'Chaussure d\'été', 'Vêtement');
 
 -- --------------------------------------------------------
 
