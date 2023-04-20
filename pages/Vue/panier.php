5a3162d88e3e61fe0a869ca2d10a83e4fbbeec81
ClementFOulon <clement.yang@outlook.fr>
2023-04-20 17:44:57 +0200

    ok samy

--- pages/Vue/panier.php
+++ pages/Vue/panier.php
@@ -1,4 +1,7 @@
 <?php
+	//Pour la connection de la bdd
+	require_once '..\..\database\config\connection.php';
+	require_once '..\..\database\config\database.php';
     session_start();
     // Si l'utilisateur n'est pas connecté, on le redirige vers la page de connexion
     if(!isset($_SESSION['UTILISATEUR'])){
@@ -24,35 +27,32 @@
 				<h1>Panier</h1>
 				<div>
 					<?php
-						// Vérifier si l'utilisateur a cliqué sur le bouton "Ajouter au panier"
 						if (isset($_POST['ajouter_au_panier'])) {
 							// Récupérer les informations du produit envoyées par le formulaire
 							$produit_id = $_POST['produit_id'];
 							$quantite = $_POST['quantite'];
-							$prix_produit = $_POST['prix_produit'];
-							$prix_total = $_POST['prix_total'];
-						
+							$prix_produit = floatval($_POST['prix_produit']);
+							
 							// Vérifier si le panier existe déjà dans la session
 							if (!isset($_SESSION['panier'])) {
 								$_SESSION['panier'] = array(); // Si non, créer un tableau vide pour le panier
 							}
-						
+							
 							// Vérifier si le produit est déjà dans le panier
 							if (isset($_SESSION['panier'][$produit_id])) {
 								// Si oui, mettre à jour la quantité et le prix total du produit dans le panier
 								$_SESSION['panier'][$produit_id]['quantite'] += $quantite;
-								$_SESSION['panier'][$produit_id]['prix_total'] += $prix_produit * $quantite;
+								$_SESSION['panier'][$produit_id]['prix_total'] += floatval($prix_produit) * $quantite;
 							} else {
 								// Si non, ajouter le produit au panier avec sa quantité et son prix total
 								$_SESSION['panier'][$produit_id] = array(
 									'quantite' => $quantite,
 									'prix_produit' => $prix_produit,
-									'prix_total' => $prix_produit * $quantite
-								);                     
+									'prix_total' => floatval($prix_produit) * $quantite
+								);
 							}
-							
 						}
-						
+
 						// Afficher le contenu du panier
 						if (isset($_SESSION['panier'])) {
 							echo '<table>';
@@ -93,7 +93,6 @@
 						
 							// Traiter les modifications de quantité ou de suppression de produits
 							if (isset($_POST['modifier_quantite'])) {
-								// Traitement de la modification de quantité
 								// Traitement de la modification de quantité
 								foreach ($_POST['quantite'] as $produit_id => $nouvelle_quantite) {
 									$prix_produit = $_SESSION['panier'][$produit_id]['prix_produit'];
