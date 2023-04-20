187f74c98a663296dd84706f6bbdabce740ecadd
RDNATOS <78343596+RDNATOS@users.noreply.github.com>
2023-04-20 14:23:02 +0200

    Exemple de comment alimenter les classes avec la BDD

--- pages/Modèle/Classes/Compte.php
+++ pages/Modèle/Classes/Compte.php
@@ -1,5 +1,11 @@
 <?
+
+require_once 'Contrat.php';
+require_once 'Panier.php';
+
+
 class Compte {
+  
     private static string $email;
     private string $motDePasse;
     private bool $abonnement;
@@ -10,7 +16,41 @@ class Compte {
     private Panier $panier;
     
   
-    function __construct(string $email, string $motDePasse,bool $abonnement,$signatureContratClient,$signatureContratVendeur,$panier) { 
+    function __construct() {
+      
+      // récupération des données de la base de données
+      require_once '..\..\..\database\config\database.php';
+      require_once '..\..\..\database\config\connection.php';
+      $sql = "SELECT * FROM Compte WHERE email = ?";
+      $stmt = $conn->prepare($sql);
+      $stmt->bind_param("s", $email);
+      $stmt->execute();
+      $result = $stmt->get_result();
+      if ($result->num_rows > 0) {
+          $row = $result->fetch_assoc();
+          $this->abonnement = $row["abonnement"];
+          $this->signatureContratClient = $row["signatureContratClient"];
+          $this->signatureContratVendeur = $row["signatureContratVendeur"];
+
+          // initialisation de $contrat
+          $contratId = $row["contratId"];
+          //On associe un contrat
+          $this->contrat = new Contrat($contratId,$this);
+
+          // initialisation de $listeMoyensPayments
+          $sql2 = "SELECT * FROM MoyenPayment WHERE email = ?";
+          $stmt2 = $conn->prepare($sql2);
+          $stmt2->bind_param("s", $email);
+          $stmt2->execute();
+          $result2 = $stmt2->get_result();
+          if ($result2->num_rows > 0) {
+              while ($row2 = $result2->fetch_assoc()) {
+                  $this->listeMoyensPayments[] = $row2["type"];
+              }
+          }
+      }
+
+
       $this->email = $email;
       $this->motDePasse = $motDePasse;
       $this ->abonnement = $abonnement;
@@ -36,6 +76,15 @@ class Compte {
     function getSignatureContratVendeur() {
         return $this->signatureContratVendeur;
     }
+
+    function getContrat() {
+      return $this->contrat;
+  }
+  
+  function getPanier() {
+    return $this->panier;
+}
+
     
   }
   
