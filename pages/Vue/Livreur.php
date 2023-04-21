63e6ab7d4d19212249433f87364f7973e8274817
ClemZi02 <clement.foulon19@gmail.com>
2023-04-21 19:49:37 +0200

    livreur

--- pages/Vue/Livreur.php
+++ pages/Vue/Livreur.php
@@ -10,7 +10,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LE NIL</title>
-    <link rel="stylesheet" href="../../css/vendeur.css">
+    <link rel="stylesheet" href="../../css/livreur.css">
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="icon" type="image/png" href="../../img/logo2.png">
 </head>
@@ -21,12 +21,44 @@
             unset($_SESSION['message']);
 		?>
         <hr> <!-- Repère visuel temporaire -->
-        <div>
-
-
-
-        
-        </div>
+        <div class="commandes">
+            <form action="pageLivreur.php" method="post">
+                    <div class="article">
+                        <button type="submit">
+                            <div>
+                                <h5>Commande #12345</h5>
+                                <p>Date de commande : 20 avril 2023 à 10h30</p>
+                                <p>Adresse de livraison : 123 rue des Fleurs, 75001 Paris</p>
+                            </div>
+                        </button>
+                        <input type="hidden" name="produit_id" value="<?php echo $row['id']; ?>">
+                    </div>
+                </form>
+                <form action="pageLivreur.php" method="post">
+                    <div class="article">
+                        <button type="submit">
+                            <div>
+                                <h5>Commande #12345</h5>
+                                <p>Date de commande : 20 avril 2023 à 10h30</p>
+                                <p>Adresse de livraison : 123 rue des Fleurs, 75001 Paris</p>
+                            </div>
+                        </button>
+                        <input type="hidden" name="produit_id" value="<?php echo $row['id']; ?>">
+                    </div>
+                </form>
+                <form action="pageLivreur.php" method="post">
+                    <div class="article">
+                        <button type="submit">
+                            <div>
+                                <h5>Commande #12345</h5>
+                                <p>Date de commande : 20 avril 2023 à 10h30</p>
+                                <p>Adresse de livraison : 123 rue des Fleurs, 75001 Paris</p>
+                            </div>
+                        </button>
+                        <input type="hidden" name="produit_id" value="<?php echo $row['id']; ?>">
+                    </div>
+                </form>
+            </div>
         <hr> <!-- Repère visuel temporaire -->
         <?php include '../../templates/footer.php'; ?>
     </div>
